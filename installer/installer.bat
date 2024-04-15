03a3f2db5e0e0eb8171e55adc9797a30b333cac3
INUGYN <116585597+INUGYN@users.noreply.github.com>
2024-04-15 10:10:18 +0200

    15/04/2024  10h10
    test rawgit

--- installer/installer.bat
+++ installer/installer.bat
@@ -1,36 +1,16 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFDJ6eSCnCleeCbYJ5e31+/m7hUQJfPc9RLz437qaJfIv40v3YZch2n9IpMcDCxQWdxGkDg==
-::YAwzuBVtJxjWCl3EqQJhSA==
-::ZR4luwNxJguZRRmh+lEkKXs=
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFDJ6eSCnCleeCbYJ5e31+/m7hUQJfPc9RLz437qaJfIv40v3YZch2n9IpOICHw9Zch6ufEExsWsi
-::YB416Ek+ZW8=
-::
-::
-::978f952a14a936cc963da21a135fa983
-set "repertoire=%~dp0"
-pushd "%repertoire%"
-powershell -file "call_batch.ps1"
+@echo off
+setlocal
+
+REM Définition de l'URL du fichier à télécharger
+set "url=https://raw.githubusercontent.com/INUGYN/SCenter/main/installer/call_batch.ps1"
+
+REM Définition du dossier temporaire
+set "folderPath=%TEMP%"
+
+REM Téléchargement du fichier depuis l'URL
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%url%', '%folderPath%\call_batch.ps1')"
+
+REM Exécution du script téléchargé
+powershell -ExecutionPolicy Bypass -File "%folderPath%\call_batch.ps1"
+
+endlocal
